27753eb380026cf1c0e9f2578980cdc6110d4aef
RIMMA <mazeevar2912@gmail.com>
2022-10-09 11:17:58 +0500

    задача 4

--- 4/Program.cs
+++ 4/Program.cs
@@ -6,3 +6,10 @@ Console.Write("Введите второе число");
 int b = int.Parse(Console.ReadLine()!);
 Console.Write("Введите третье число");
 int c = int.Parse(Console.ReadLine()!);
+int max = a;
+if (a>max) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+Console.Write("max=");
+Console.WriteLine(max);
+
