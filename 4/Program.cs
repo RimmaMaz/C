61f63fd9d811e050c166889ab554a2c2115bbf58
RIMMA <mazeevar2912@gmail.com>
2022-10-09 10:13:32 +0500

    созд игнор в папке С#

---
+++ 4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.Write("Введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число");
+int c = int.Parse(Console.ReadLine()!);
