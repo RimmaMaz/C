65ff3cce399a24d6e19395b45c53317a19dd9a51
RIMMA <mazeevar2912@gmail.com>
2022-10-11 09:25:17 +0500

    задача 6

---
+++ 6/Program.cs
@@ -0,0 +1,23 @@
+﻿// на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число...");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a%2==0);
+{
+    Console.Write($"{a}-");
+    Console.WriteLine("четное");
+}
+
+else
+{
+    Console.Write($"{a}-");
+    Console.WriteLine("не является четным");
+}
+
+
+
+
+
+
+
