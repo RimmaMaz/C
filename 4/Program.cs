f7a0e7bb8d5462dc6dc8359347bfc82a02f07c5b
RIMMA <mazeevar2912@gmail.com>
2022-10-14 11:26:21 +0500

    задача 8

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Принимает на вход три числа и выдаёт максимальное из этих чисел
 
-Console.Write("Введите первое число");
+Console.WriteLine("Введите первое число");
 int a = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число");
 int b = int.Parse(Console.ReadLine()!);
--- 8/Program.cs
+++ 8/Program.cs
@@ -1,2 +1,11 @@
 ﻿//на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
 
+Console.WriteLine("Введите число...");
+int N = int.Parse(Console.ReadLine()!);
+int x=1;
+while(x<=N) if (x % 2==0)
+{
+    Console.Write($"x");
+    x=x+1;
+}
+
